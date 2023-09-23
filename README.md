8e41b97dd36cd8ad4764b8d46635ddc805caaaa1
Dopefish <artyom.movchan@gmail.com>
2023-09-23 14:12:03 +0300

    added *.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # movies-explorer-api
+
+
+Api доступно по адресу
+
+https://api.redox.nomoredomainsicu.ru/
+
+`npm run start` — запускает сервер   
+`npm run dev` — запускает сервер с hot-reload
--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
+const cors = require('cors');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
+
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 1000,
+});
+
+app.use(requestLogger);
+app.use(limiter);
+
+app.use(helmet());
 
 const router = require('./routes');
 
@@ -15,7 +33,11 @@ mongoose.connect(DB_URL, {
 app.use(express.json());
 app.use(router);
 
+app.use(errorLogger);
+
 app.use(errors());
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 });
